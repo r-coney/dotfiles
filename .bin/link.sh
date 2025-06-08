afe132ce5fe0b99fbc308361480844be0b485536
ryosei <rstar0823@icloud.com>
2025-06-08 19:29:07 +0900

    remove: 整理のため、nvimを削除

--- .bin/link.sh
+++ .bin/link.sh
@@ -23,42 +23,42 @@ function link_dotfiles() {
 }
 
 # Create directory for neovim
-function create_nvim_directory() {
-  info "Creating nvim symlinks"
-  if [ ! -d "${HOME}/.config/nvim" ]; then
-     info "Creating ~/.config/nvim"
-     mkdir -p "${HOME}/.config/nvim"
-     mkdir -p "${HOME}/.config/nvim/after/plugin"
-     mkdir -p "${HOME}/.config/nvim/lua"
-     mkdir -p "${HOME}/.config/nvim/plugin"
-  fi
-}
+# function create_nvim_directory() {
+#   info "Creating nvim symlinks"
+#   if [ ! -d "${HOME}/.config/nvim" ]; then
+#      info "Creating ~/.config/nvim"
+#      mkdir -p "${HOME}/.config/nvim"
+#      mkdir -p "${HOME}/.config/nvim/after/plugin"
+#      mkdir -p "${HOME}/.config/nvim/lua"
+#      mkdir -p "${HOME}/.config/nvim/plugin"
+#   fi
+# }
 
 # Create symbolic link for neovim
-function link_nvim() {
-  # init.lua
-  if [ "$1" == 'init' ]; then
-    init_lua=${DOTFILES}/nvim/init.lua
-    if [ ! -e "${HOME}/.config/nvim/init.lua" ]; then
-      ln -s "$init_lua" ~/.config/nvim/init.lua
-    else
-      info "~/.config/nvim/init.lua already exists... Skipping"
-    fi
+# function link_nvim() {
+#   # init.lua
+#   if [ "$1" == 'init' ]; then
+#     init_lua=${DOTFILES}/nvim/init.lua
+#     if [ ! -e "${HOME}/.config/nvim/init.lua" ]; then
+#       ln -s "$init_lua" ~/.config/nvim/init.lua
+#     else
+#       info "~/.config/nvim/init.lua already exists... Skipping"
+#     fi
 
-    return 0
-  fi
+#     return 0
+#   fi
 
-  lua_files=$(find "${DOTFILES}/nvim/${1}" -maxdepth 1 -name '*.lua')
-  for lua in $lua_files; do
-    target="${HOME}/.config/nvim/$1/$(basename "$lua")"
-    if [ -e "$target" ]; then
-      info "~${target#$HOME} already exists... Skipping."
-    else
-      info "Creating symlink for ${lua}"
-      ln -s "$lua" "$target"
-    fi
-  done
-}
+#   lua_files=$(find "${DOTFILES}/nvim/${1}" -maxdepth 1 -name '*.lua')
+#   for lua in $lua_files; do
+#     target="${HOME}/.config/nvim/$1/$(basename "$lua")"
+#     if [ -e "$target" ]; then
+#       info "~${target#$HOME} already exists... Skipping."
+#     else
+#       info "Creating symlink for ${lua}"
+#       ln -s "$lua" "$target"
+#     fi
+#   done
+# }
 
 # Create symbolic links
 function symlinks() {
@@ -66,11 +66,11 @@ function symlinks() {
 
   link_dotfiles
 
-  create_nvim_directory
-  link_nvim 'init'
-  link_nvim 'after/plugin'
-  link_nvim 'lua'
-  link_nvim 'plugin'
+  # create_nvim_directory
+  # link_nvim 'init'
+  # link_nvim 'after/plugin'
+  # link_nvim 'lua'
+  # link_nvim 'plugin'
 }
 
 symlinks
