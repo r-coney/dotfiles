9bdb2ce437904478fb7aeda017d89fb96fe537e8
ryosei <rstar0823@icloud.com>
2025-06-10 13:04:26 +0900

    feat: neovimのシンボリックリンクを作成するよう変更

--- setUp/link.sh
+++ setUp/link.sh
@@ -4,8 +4,9 @@ readonly DOTFILES=$(pwd)
 readonly SCRIPT_DIR="${DOTFILES}/.scripts"
 source "${SCRIPT_DIR}/_common.sh"
 
-# Create symbolic link for dotfiles
-function link_dotfiles() {
+# Create symbolic links
+function symlinks() {
+	title "Creating symlinks"
 	info "Creating dotfiles symlinks"
 	# .Brewfileのシンボリックリンクを作成
 	if [ -e "${HOME}/.Brewfile" ]; then
@@ -30,57 +31,14 @@ function link_dotfiles() {
 		info "Adding symlink to .scripts at ${HOME}/.scripts"
 		ln -s "${DOTFILES}/.scripts" "${HOME}/.scripts"
 	fi
-}
 
-# Create directory for neovim
-# function create_nvim_directory() {
-#   info "Creating nvim symlinks"
-#   if [ ! -d "${HOME}/.config/nvim" ]; then
-#      info "Creating ~/.config/nvim"
-#      mkdir -p "${HOME}/.config/nvim"
-#      mkdir -p "${HOME}/.config/nvim/after/plugin"
-#      mkdir -p "${HOME}/.config/nvim/lua"
-#      mkdir -p "${HOME}/.config/nvim/plugin"
-#   fi
-# }
-
-# Create symbolic link for neovim
-# function link_nvim() {
-#   # init.lua
-#   if [ "$1" == 'init' ]; then
-#     init_lua=${DOTFILES}/nvim/init.lua
-#     if [ ! -e "${HOME}/.config/nvim/init.lua" ]; then
-#       ln -s "$init_lua" ~/.config/nvim/init.lua
-#     else
-#       info "~/.config/nvim/init.lua already exists... Skipping"
-#     fi
-
-#     return 0
-#   fi
-
-#   lua_files=$(find "${DOTFILES}/nvim/${1}" -maxdepth 1 -name '*.lua')
-#   for lua in $lua_files; do
-#     target="${HOME}/.config/nvim/$1/$(basename "$lua")"
-#     if [ -e "$target" ]; then
-#       info "~${target#$HOME} already exists... Skipping."
-#     else
-#       info "Creating symlink for ${lua}"
-#       ln -s "$lua" "$target"
-#     fi
-#   done
-# }
-
-# Create symbolic links
-function symlinks() {
-	title "Creating symlinks"
-
-	link_dotfiles
-
-	# create_nvim_directory
-	# link_nvim 'init'
-	# link_nvim 'after/plugin'
-	# link_nvim 'lua'
-	# link_nvim 'plugin'
+	# .config/nvimのシンボリックリンクを作成
+	if [ -d "${HOME}/.config/nvim" ]; then
+		info "~/.config/nvim already exists... Skipping."
+	else
+		info "Adding symlink to .config/nvim at ${HOME}/.config/nvim"
+		ln -s "${DOTFILES}/nvim" "${HOME}/.config/nvim"
+	fi
 }
 
 symlinks
