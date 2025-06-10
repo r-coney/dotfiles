d3e34728282d27bb03aa9efec2ad20f7b7fc5d50
ryosei <rstar0823@icloud.com>
2025-06-10 13:20:08 +0900

    fix: neovimのkeymapsが読み込まれていなかったため、修正

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
 require("config.lazy")
+require("config.keymaps")
 
 vim.cmd('language en_US.UTF-8')
 
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,50 +4,53 @@
 
 -- leader: プラグイン系のprefix
 -- Ctrl:   通常キーのprefix
-
+vim.g.mapleader = " "
 local keymap = vim.keymap
-vim.g.mapleader = ' '
 
 if vim.g.vscode then
   keymap.set("n", "<leader>s", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>")
   keymap.set("n", "<leader>S", "<Cmd>call VSCodeNotify('workbench.view.search')<CR>")
-  keymap.set('n', '<leader>f', "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>")
-  keymap.set('n', '<leader>b', "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
-  keymap.set('n', '<C-h>', "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
-  keymap.set('n', '<C-l>', "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
-  keymap.set('n', '<C-W>', "<Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>")
+  keymap.set("n", "<leader>f", "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>")
+  keymap.set("n", "<leader>b", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
+  keymap.set("n", "<C-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+  keymap.set("n", "<C-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+  keymap.set("n", "<C-W>", "<Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>")
 else
-  keymap.set('i','jj','<Esc>')
-  keymap.set('n', '<leader>s', function() require("telescope.builtin").find_files { hidden = true } end, {noremap = true})
-  keymap.set('n', '<leader>S', function() require("telescope.builtin").live_grep { hidden = true } end, {noremap = true})
-  keymap.set('n', '<leader>f', '<Cmd>Fern .<CR>')
-  keymap.set('n', '<C-h>', "<Cmd>bprevious<CR>")
-  keymap.set('n', '<C-l>', "<Cmd>bnext<CR>")
-  keymap.set('n', '<C-w>', "<Cmd>bdelete<CR>")
+  -- ノーマル＆インサートモード
+  keymap.set("i", "jj", "<Esc>", { desc = "jj to escape" })
+  keymap.set("n", "<leader>s", function()
+    require("telescope.builtin").find_files({ hidden = true })
+  end, { noremap = true, silent = true, desc = "Find files (telescope)" })
+  keymap.set("n", "<leader>S", function()
+    require("telescope.builtin").live_grep({ hidden = true })
+  end, { noremap = true, silent = true, desc = "Live grep (telescope)" })
+  keymap.set("n", "<leader>f", "<Cmd>Fern .<CR>", { desc = "Open fern" })
+  keymap.set("n", "<C-h>", "<Cmd>bprevious<CR>", { desc = "Previous buffer" })
+  keymap.set("n", "<C-l>", "<Cmd>bnext<CR>", { desc = "Next buffer" })
+  keymap.set("n", "<C-w>", "<Cmd>bdelete<CR>", { desc = "Close buffer" })
 end
 
--- 画面分割
-keymap.set('n', 'ss', ':split <Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
--- アクティブウィンドウの移動
-keymap.set('n', 'sh', '<C-w>h')
-keymap.set('n', 'sk', '<C-w>k')
-keymap.set('n', 'sj', '<C-w>j')
-keymap.set('n', 'sl', '<C-w>l')
+-- ウィンドウ分割
+keymap.set("n", "ss", ":split<Return><C-w>w", { desc = "Horizontal split" })
+keymap.set("n", "sv", ":vsplit<Return><C-w>w", { desc = "Vertical split" })
 
--- Emacss風
-keymap.set('i', '<C-f>', '<Right>')
--- 設定ファイルを開く
--- keymap.set('n','<F1>',':edit $MYVIMRC<CR>')
+-- ウィンドウ移動
+keymap.set("n", "sh", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "sk", "<C-w>k", { desc = "Move to upper window" })
+keymap.set("n", "sj", "<C-w>j", { desc = "Move to lower window" })
+keymap.set("n", "sl", "<C-w>l", { desc = "Move to right window" })
 
--- 削除でコピーされないように変更
-keymap.set('n', 'd', '"_d')
+-- Emacs風のカーソル移動（インサート）
+keymap.set("i", "<C-f>", "<Right>", { desc = "Move right in insert mode" })
 
--- 移動
-keymap.set('n', '<S-h>', '0')
-keymap.set('n', '<S-l>', '$')
-keymap.set('n', '<C-j>', '10j')
-keymap.set('n', '<C-k>', '10k')
+-- コピーせず削除
+keymap.set("n", "d", '"_d', { desc = "Delete without yanking" })
 
--- terminal mode
-keymap.set('t', '<ESC>', '<C-\\><C-n>')
+-- 行頭・行末・高速移動
+keymap.set("n", "<S-h>", "0", { desc = "Move to line start" })
+keymap.set("n", "<S-l>", "$", { desc = "Move to line end" })
+keymap.set("n", "<C-j>", "10j", { desc = "Move down 10 lines" })
+keymap.set("n", "<C-k>", "10k", { desc = "Move up 10 lines" })
+
+-- ターミナルモード → ノーマルに戻る
+keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Escape terminal mode" })
