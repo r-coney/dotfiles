591c6f9fb2530a010ef43f8a6b3eb64210c4ef21
ryosei <rstar0823@icloud.com>
2025-06-11 09:34:19 +0900

    fix: neovim構成スクリプトに不具合があったため修正

--- homebrew/neovim0.9.5.sh
+++ homebrew/neovim0.9.5.sh
@@ -3,16 +3,31 @@
 
 #!/bin/bash
 
+readonly DOTFILES=$(pwd)
 readonly SCRIPT_DIR="${HOME}/.scripts"
 source "${SCRIPT_DIR}/_common.sh"
 
 title 'Creating custom Homebrew formula for neovim@0.9.5'
 if [ -f "$(brew --repo my/local)/Formula/neovim@0.9.5.rb" ]; then
-  info "Custom Homebrew formula for neovim@0.9.5 already exists, skipping creation."
+	# 上書きするか確認
+	read -p "neovim@0.9.5 formula already exists. Do you want to overwrite it? (y/n): " choice
+	case "$choice" in
+	[Yy]*)
+		info "Overwriting existing neovim@0.9.5 formula"
+		rm "$(brew --repo my/local)/Formula/neovim@0.9.5.rb"
+		ln -s "${DOTFILES}/homebrew/neovim@0.9.5.rb" "$(brew --repo my/local)/Formula/neovim@0.9.5.rb"
+		;;
+	[Nn]*)
+		info "Skipping overwrite of existing neovim@0.9.5 formula"
+		;;
+	*)
+		info "Invalid choice. Skipping overwrite of existing neovim@0.9.5 formula"
+		;;
+	esac
 else
-  info "Creating custom Homebrew formula for neovim@0.9.5"
-  brew tap-new my/local
-  ln -s ~/neovim@0.9.5.rb "$(brew --repo my/local)/Formula/neovim@0.9.5.rb"
+	info "Creating custom Homebrew formula for neovim@0.9.5"
+	brew tap-new my/local
+	ln -s "${DOTFILES}/homebrew/neovim@0.9.5.rb" "$(brew --repo my/local)/Formula/neovim@0.9.5.rb"
 fi
 
 success 'Done.'
