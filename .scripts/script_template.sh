90b90e679d734a9aa74b47bdd044b52ce3c6f1da
r-coney <rstar0823@icloud.com>
2025-06-12 21:45:18 +0900

    feat: スクリプトテンプレートを更新

--- .scripts/script_template.sh
+++ .scripts/script_template.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-
-readonly DOCUMENTS_PATH="${HOME}/Projects"
-readonly SCRIPT_DIR="${HOME}/dotfiles/.bin"
+readonly SCRIPT_DIR="${HOME}/.scripts"
 source "${SCRIPT_DIR}/_utils.sh"
 
 # 使用方法
 function usage {
-  cat <<EOF
+	cat <<EOF
 $(basename ${0}) is a tool for ...
 
 USAGE:
-    $(basename ${0}) <CONTENTS>
+	$(basename ${0}) <CONTENTS>
 EOF
 }
 
 # 引数がない場合
 if [ $# = 0 ]; then
-  usage
-  exit
+	usage
+	exit
 fi
 
 # オプションが指定された場合の処理
-while getopts "h" optKey; do
-  case "$optKey" in
-  *)
-    usage
-    exit
-    ;;
-  esac
+while getopts "xzyh" optKey; do
+	case "$optKey" in
+	x)
+		info "Option X selected"
+		;;
+	y)
+		info "Option Y selected"
+		;;
+	z)
+		info "Option Z selected"
+		;;
+	*)
+		usage
+		exit
+		;;
+	esac
 done
 
 success "Done."
