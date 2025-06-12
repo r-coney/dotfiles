c3de6bb79663447dc02340c73584c25e248a10cf
r-coney <rstar0823@icloud.com>
2025-06-12 21:30:53 +0900

    refactor: gitReviewCheckout.shのメッセージが見やすくなるよう変更

--- .scripts/gitReviewCheckout.sh
+++ .scripts/gitReviewCheckout.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-# Usage: ./switch_branch.sh branch_name
+readonly SCRIPT_DIR="${HOME}/.scripts"
+source "${SCRIPT_DIR}/_common.sh"
 
 BRANCH=$1
 
 if [ -z "$BRANCH" ]; then
-  echo "Usage: $0 branch_name"
+  error "Usage: $0 branch_name"
   exit 1
 fi
 
 # Fetch the specific branch from remote
-echo "Fetching branch '$BRANCH' from remote..."
+title "Fetching branch '$BRANCH' from remote..."
 git fetch origin "$BRANCH"
 
 # Check if the local branch exists
 if git show-ref --verify --quiet refs/heads/"$BRANCH"; then
-  echo "Local branch '$BRANCH' exists. Checking out and pulling latest changes..."
+  info "Local branch '$BRANCH' exists. Checking out and pulling latest changes..."
   git checkout "$BRANCH" && git pull origin "$BRANCH"
 else
   # Check if the remote branch exists
   if git show-ref --verify --quiet refs/remotes/origin/"$BRANCH"; then
-    echo "Remote branch '$BRANCH' found. Checking out new local branch..."
+    info "Remote branch '$BRANCH' found. Checking out new local branch..."
     git checkout -b "$BRANCH" origin/"$BRANCH"
   else
-    echo "Error: Branch '$BRANCH' does not exist on remote."
+    error "Error: Branch '$BRANCH' does not exist on remote."
     exit 1
   fi
 fi
